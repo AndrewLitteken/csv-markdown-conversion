73022ffab0f987e2e341cfc3aa98f8e4cc6d5dca
Andrew Litteken <andrew.litteken@gmail.com>
2017-08-11 16:29:33 -0500

    updating small fixes and README

--- README.md
+++ README.md
@@ -1,7 +1,37 @@
 # CSV to Markdown Converter #
 
-A simple script to convert a `.csv` file into a nice formatted Markdown table in plain text and when interpreted.
+A simple script to convert a `.csv` file into a nicely formatted Markdown table in plain text and when interpreted.
 
 Written in Python and compatible with both Python 2.7 and Python 3.
 
-Currently only delimiter supported is a comma, will be updated further with a more full formatting construct.
+Can support any delimeter, but a comma is supported by default
+
+### Command Line Options ###
+`-h` `--help` : Show usage statement
+`-v` `--verbose` : Show verbose information at runtime
+`-s` `--same_name` : Use input file name with file ending "md"    
+ 
+ `-i` `--input` input\_file : use given file for input
+ `-o` `--output` output\_file : use given file for output
+ `-d` `--delimeter` delimeter : use the given delimeter
+ `-f` `--format` format\_file : use the given file for formatting
+
+### Formatting File ###
+A formatting file can be provided to specify **bolding**, __italics__ and `code` in various cells.
+
+The style is defined by the keywords `bold`, `italics` and `code`.  In a statement these can be placed at any time and multiple styles can be used.
+
+The location is defined by keywords `row` and `col`.  These must be followed by a number in range to define the location.  This keyword and number pair can also be placed at any place in the statement.  `row` or `col` can not be used if the `all` keyword is used following the number.  This defines that an entire row or column will be styled as specified.
+
+There are other special keywords: `title`, `bottom`, `start`, `end`.  These define a location similar to `all`.  `title` will style the top cell, `bottom` the bottom cell, `start` the leftmost cell in the row, and `end` the rightmost cell in the row.  All except for `end` can be used at any time.  The row must be specified previously in the statement for `end` to be used.
+
+#### Examples ####
+```
+bold row 5 all
+bold title col 6 italics
+code row 10 col 3
+row 10 bold end
+```
+
+
+
--- csv-to-markdown.py
+++ csv-to-markdown.py
@@ -4,7 +4,9 @@ import getopt
 
 def usage(code=0):
 	print('''
-    usage: csv-to-markdown.py [-hv] [-i --input input_file] [-s --samename | -o --output output_file] [-d --delimeter delimeter] [-f --format format_file] [--help]
+    usage: csv-to-markdown.py [-hv] [-i --input input_file] [-s --samename | -o
+		   --output output_file] [-d --delimeter delimeter] [-f --format format_file]
+ 		   [--help]
     
     -h --help : Show usage statement
     -v --verbose : Show verbose information at runtime
@@ -17,239 +19,244 @@ def usage(code=0):
 	''')
 	sys.exit(code)
 
-def error_message(code, line_num, err):
-	sys.stderr.write("Line "+str(line_num)+": "+err+"\n")
+def error_handler(code, line_num, err):
+	sys.stderr.write("Line " + str(line_num) + ": " + err + "\n")
 	sys.exit(code) 
 
-def format_item(item, col, row):
+def format_item(formats, item, col, row):
 	if (col, row) in formats:
 		style = formats[(col, row)]
 		for style_type in style:
 			if style_type == 'bold':
-				item = '**'+item+'**'
-				if len(item) > maximum[col]:
-					maximum[col] = len(item)
+				item = '**' + item + '**'
 			if style_type == 'italics':
-				item = '__'+item+'__'
-				if len(item) > maximum[col]:
-					maximum[col] = len(item)
+				item = '__' + item + '__'
 			if style_type == 'code':
-				item = '`'+item+'`'
-				if len(item) > maximum[col]:
-					maximum[col] = len(item)
+				item = '`' + item + '`'
 	return item
+def main():
+	# Variables that shift with formatting
+	delim = ','
+	output_loc = '-'
+	same_name = False
+	input_loc = '-'
+	formatting = False
+	format_file = "-"
+	verbose = False
 
-# Variables that shift with formatting
-delim = ','
-output_loc = '-'
-same_name = False
-input_loc = '-'
-formatting = False
-format_file = "-"
-verbose = False
+	# Command line options
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], 'hsvd:i:o:f:', ["help", "verbose", "same_name", "delimeter=", "format=", "input=", "output="])
+	except getopt.GetoptError as err:
+		print(err)
+		usage(2)
 
-# Command line options
-try:
-	opts, args = getopt.getopt(sys.argv[1:], 'hsvd:i:o:f:', ["help", "verbose", "same_name", "delimeter=", "format=", "input=", "output="])
-except getopt.GetoptError as err:
-	print(err)
-	usage(2)
-
-#Parse Command Line
-for o, a in opts:
-	if o in ("-s", "--same_name"):
-		same_name = True
-	elif o in ("-f", "--format"):
-		formatting = True
-		format_file = a
-	elif o in ("-i", "--input"):
-		input_loc = a
-	elif o in ("-o", "--output"):
-		output_loc = a
-	elif o in ("-v", "--verbose"):
-		verbose = True
-	elif o in ("-d", "--delimeter"):
-		delim = a
-	elif o in ("-h", "--help"):
-		usage(0)
-	else:
-		assert False, "unhandled option"
-
-# Create destination file if same as input and output not specified
-if same_name and output_loc == '-' and input_loc != '-':
-	base_name = os.path.basename(input_loc)
-	output_loc = os.path.split(input_loc)[0]+os.path.splitext(base_name)[0] + '.md'
-elif output_loc != '-' and same_name:
-	sys.stderr.write("Only -s or -o can be used at one time\n")
-	usage(3)
-elif same_name and input_loc == '-':
-	sys.stderr.write("Input file must be specified for -s\n")
-	sys.exit(5)
-
-# Arrays for maximum characters per column and information
-if input_loc == '-':
-	r = sys.stdin
-else:
-	r = open(input_loc, 'r')
-
-maximum = []
-max_length = 0
-rows = []
-data = []
-
-
-# Get information
-for line in r:
-	items = line.rstrip("\n, ").split(delim)
-	data.append(items)
-	if max_length < len(items):
-		max_length = len(items)
-
-if r is not sys.stdin:
-	r.close()
-
-# read in fomratting file
-if formatting:
-	f = open(format_file, 'r')
-	
-	# Formatting infomration
-	special_locations = ()
-	style = False
-	format_options = ('bold', 'italics', 'code')
-	format_locations = ('row', 'col')
-	format_special_index = ('title', 'bottom', 'end', 'start')
-	format_command = ('remove')
-	formats = {}
-
-	# Get line from file
-	for num, line in enumerate(f):
-		newline = line.strip().split()
-		key=['','']
-		value=set()
-		
-		# Parse the line
-		for index, word in enumerate(newline):
-			word = word.lower()
-			error = ''
-			
-			# Look for style
-			if word in format_options:
-				value.add(word)
-
-			# Look to see if row or column indicated
-			elif word in format_locations:
-				digit=0
-
-				# Check to see if following item is a digit
-				if newline[index+1].isdigit():
-					digit = int(newline[index+1])
-					
-					# Assign location if in range
-					if word == 'row' and digit < len(data):
-						key[1] = digit
-					#Assign location if in range
-					elif word == 'col' and digit < len(data[digit]):
-						key[0] = digit
-					# set error
-					else: 
-						error = word+" out of range"
-					
-					# Check for "all" keyword and set in dictionary
-					if newline[index+2] == "all" and error == '':
-						if word == 'row':
-							key_index = 0
-							length =  len(data[digit])
-						else:
-							key_index = 1
-							len(data)
-						for i in range(0,length):
-							key[key_index] = i
-							if (key[0],key[1]) in formats:
-								for styles in value:
-									formats[(key[0],key[1])].add(styles)
-							elif str(key[0]).isdigit() and str(key[1]).isdigit():
-								formats[(key[0],key[1])] = value
-					
-					# Check to see if an unsupported keyword has been given
-					elif error != '-' and newline[index+2] not in format_locations and newline[index+2] not in format_special_index and newline[index+2] not in format_command and newline[index+2] not in format_options:
-						error = newline[index+2]+" is an unsupported keyword"
-			# Check for special keywords
-			elif word in format_special_index:
-				if word == 'title':
-					key[1] = 0
-				elif word == 'bottom':
-					key[1] = len(data)-1
-				elif word == 'start':
-					key[0] = 0
-				elif word == 'end': # Set only if row defined
-					if str(key[1]).isdigit():
-						key[0] = len(data[key[1]])-1
-					else:
-						error = 'row must be defined before using "end"'
-			
-			# If argument is number, check context
-			elif word.isdigit():
-				if newline[index-1] not in format_locations:
-					error = word+" cannot be attributed to a command"
-				else:
-					continue
-			elif word == 'all':
-				continue
-			else:
-				error = "Command was not found"
-			
-			# Display error message
-			if error != '':
-				error_message(4, num+1, error)
-		
-		# Add to the dictioary if possible	
-		if (key[0],key[1]) in formats:
-			for styles in value:
-				formats[(key[0],key[1])].add(styles)
-		elif str(key[0]).isdigit() and str(key[1]).isdigit():
-			formats[(key[0],key[1])] = value
-	
-	f.close()
-
-# Parse information
-for row_index, row in enumerate(data):
-	items = []
-	
-	for col_index, item in enumerate(row):
-		if len(maximum) >= col_index+1: # if length of max array large enough
-			if maximum[col_index] < len(item): # check and replace max if needed
-				maximum[col_index] = len(item)
+	#Parse Command Line
+	for o, a in opts:
+		if o in ("-s", "--same_name"):
+			same_name = True
+		elif o in ("-f", "--format"):
+			formatting = True
+			format_file = a
+		elif o in ("-i", "--input"):
+			input_loc = a
+		elif o in ("-o", "--output"):
+			output_loc = a
+		elif o in ("-v", "--verbose"):
+			verbose = True
+		elif o in ("-d", "--delimeter"):
+			delim = a
+		elif o in ("-h", "--help"):
+			usage(0)
 		else:
-			maximum.append(len(item)) # add new entry if not large enough
-	
-		if formatting:
-			item = format_item(item, col_index, row_index)	
+			assert False, "unhandled option"
+
+	# Create destination file if same as input and output not specified
+	if same_name and output_loc == '-' and input_loc != '-':
+		base_name = os.path.basename(input_loc)
+		output_loc = os.path.split(input_loc)[0]+os.path.splitext(base_name)[0] + '.md'
+	elif output_loc != '-' and same_name:
+		sys.stderr.write("Only -s or -o can be used at one time\n")
+		usage(3)
+	elif same_name and input_loc == '-':
+		sys.stderr.write("Input file must be specified for -s\n")
+		sys.exit(5)
+
+	# Arrays for maximum characters per column and information
+	if input_loc == '-':
+		r = sys.stdin
+	else:
+		r = open(input_loc, 'r')
+
+	maximum = []
+	max_length = 0
+	rows = []
+	data = []
+
+
+	# Get information
+	for line in r:
+		items = line.rstrip("\n, ").split(delim)
+		data.append(items)
+		if max_length < len(items):
+			max_length = len(items)
+
+	if r is not sys.stdin:
+		r.close()
+
+	# read in fomratting file
+	if formatting:
+		f = open(format_file, 'r')
 		
-		items.append(item) # append data to item array
+		# Formatting infomration
+		special_locations = ()
+		style = False
+		format_options = {'bold', 'italics', 'code'}
+		format_locations = {'row', 'col'}
+		format_special_index = {'title', 'bottom', 'end', 'start'}
+		format_command = {'remove'}
+		formats = {}
 
-	rows.append(items) # add row of info to rows array
-	
-# Put table into destination file
-if output_loc and output_loc!='-':
-	w = open(output_loc, 'w')
-else:
-	w = sys.stdout
+		# Get line from file
+		for num, line in enumerate(f):
+			newline = line.strip().split()
+			key=['','']
+			value=set()
+			
+			# Parse the line
+			for index, word in enumerate(newline):
+				word = word.lower()
+				error = ''
+				
+				# Look for style
+				if word in format_options:
+					value.add(word)
 
-# Print out 
-for num, row in enumerate(rows):
-	if num == 1:
-		for maxi in maximum:
-			w.write('|{:-^{width}}'.format('',width=maxi))
-		w.write('|\n')	
+				# Look to see if row or column indicated
+				elif word in format_locations:
+					digit=0
 
-	for index, item in enumerate(row):
-		w.write('|{:^{width}}'.format(item,width=maximum[index]))
-	
-	if index+1 < max_length:
-		for num in range(index+1,max_length):
-			w.write('|{:^{width}}'.format('',width=maximum[num]))
-	
-	w.write('|\n')
+					# Check to see if following item is a digit
+					if newline[index+1].isdigit():
+						digit = int(newline[index+1])
+						
+						# Assign location if in range
+						if word == 'row' and digit < len(data):
+							key[1] = digit
+						#Assign location if in range
+						elif word == 'col' and digit < len(data[digit]):
+							key[0] = digit
+						# set error
+						else: 
+							error = word+" out of range"
+							error_handler(4, num + 1, error)
+						
+						# Check for "all" keyword and set in dictionary
+						if newline[index+2] == "all" and error == '':
+							if word == 'row':
+								key_index = 0
+								length =  len(data[digit])
+							else:
+								key_index = 1
+								len(data)
+							for i in range(0,length):
+								key[key_index] = i
+								if (key[0],key[1]) in formats:
+									for styles in value:
+										formats[(key[0],key[1])].add(styles)
+								elif str(key[0]).isdigit() and str(key[1]).isdigit():
+									formats[(key[0],key[1])] = value
+						
+						# Check to see if an unsupported keyword has been given
+						elif error != '-' and newline[index + 2] not in format_locations and newline[index + 2] not in format_special_index and newline[index + 2] not in format_command and newline[index + 2] not in format_options:
+							error = newline[index + 2] + " is an unsupported keyword"
+							error_handler(4, num + 1, error)
+				
+				# Check for special keywords
+				elif word in format_special_index:
+					if word == 'title':
+						key[1] = 0
+					elif word == 'bottom':
+						key[1] = len(data) - 1
+					elif word == 'start':
+						key[0] = 0
+					elif word == 'end': # Set only if row defined
+						if str(key[1]).isdigit():
+							key[0] = len(data[key[1]]) - 1
+						else:
+							error = 'row must be defined before using "end"'
+							error_handler(4, num + 1, error)
+				
+				# If argument is number, check context
+				elif word.isdigit():
+					if newline[index - 1] not in format_locations:
+						error = word+" cannot be attributed to a command"
+						error_handler(4, num + 1, error)
+					else:
+						continue
+				elif word == 'all':
+					continue
+				else:
+					error = "Command was not found"
+					error_handler(4, num + 1, error)
+				
+				# Display error message
+				if error != '':
+					error_handler(4, num + 1, error)
+			
+			# Add to the dictioary if possible	
+			if (key[0],key[1]) in formats:
+				for styles in value:
+					formats[(key[0],key[1])].add(styles)
+			elif str(key[0]).isdigit() and str(key[1]).isdigit():
+				formats[(key[0],key[1])] = value
+		
+		f.close()
 
-if w is not sys.stdout:
-	w.close()
+	# Parse information
+	for row_index, row in enumerate(data):
+		items = []
+		
+		for col_index, item in enumerate(row):
+			if len(maximum) >= col_index + 1: # if length of max array large enough
+				if maximum[col_index] < len(item): # check and replace max if needed
+					maximum[col_index] = len(item)
+			else:
+				maximum.append(len(item)) # add new entry if not large enough
+		
+			if formatting:
+				item = format_item(formats, item, col_index, row_index)	
+				if len(item) > maximum[col_index]:
+					maximum[col_index] = len(item)
+			
+			items.append(item) # append data to item array
+
+		rows.append(items) # add row of info to rows array
+		
+	# Put table into destination file
+	if output_loc and output_loc!='-':
+		w = open(output_loc, 'w')
+	else:
+		w = sys.stdout
+
+	# Print out 
+	for num, row in enumerate(rows):
+		if num == 1:
+			for maxi in maximum:
+				w.write('|{:-^{width}}'.format('',width=maxi))
+			w.write('|\n')	
+
+		for index, item in enumerate(row):
+			w.write('|{:^{width}}'.format(item,width=maximum[index]))
+		
+		if index+1 < max_length:
+			for num in range(index+1,max_length):
+				w.write('|{:^{width}}'.format('',width=maximum[num]))
+		
+		w.write('|\n')
+
+	if w is not sys.stdout:
+		w.close()
+
+if __name__ == '__main__':
+	main()
